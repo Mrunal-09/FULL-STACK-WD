aa84bac2f25c3edc4198c2f91ae137c72f32ac6a
Mrunal-09 <gosavimrunal36@gmail.com>
2021-10-22 07:48:20 +0530

    JS data types and operators

---
+++ app.js
@@ -0,0 +1,75 @@
+console.log("Hello World")
+// console is a object which has various methods
+// by console.log you can view in your browser->inspect->console
+console.warn("Hello World")
+// .warn for warning
+console.error("Hello World")
+console.clear("Hello World")
+// clears everything
+// other programming language-primitive data type-not derived fom any other data type..cannot be change .....non primitive data type
+// js-data type is object-nonprimitive
+// js-data type is not object-primitive
+// primitive data types-string,number,boolean
+console.log(typeof("hello"))
+console.log(typeof(667))
+console.log(typeof(88.9))
+ console.log(typeof(true))
+//  variable
+var age=null
+console.log(typeof(age))
+console.log(name)
+// null is object
+// undefined is not giving value
+// nonprimitive data types-array
+var arr=[1,'s',4]
+console.log(arr)
+console.log(typeof(arr))
+console.log(arr.length)
+arr.push(7)
+console.log(arr)
+console.log(arr.length)
+const n="mrunal"
+console.log(n)
+// operators
+// arithmetic
+var a = 1
+var b = 2
+console.log(a + b)
+console.log(a - b)
+console.log(a / b)
+console.log(a * b)
+console.log(a ** b)
+// ** is power
+// assignment
+console.log(a % b)
+b+=10
+console.log(b)
+b++
+console.log(b)
+b-=1
+console.log(b)
+console.log(++b)
+
+// comparison operators
+console.log(b)
+console.log(a)
+console.log(a>b)
+console.log(a<b)
+console.log(a==b)
+console.log(a!=b)
+var a=1
+var b=2
+var c="2"
+console.warn(c==b)
+console.log(c===b)
+// imp ==doesnot check typeof
+// logical operators
+console.log('true && true',true && true)
+console.log('true && false',true && false)
+console.log('false && true',false && true)
+console.log('false && false',false && false)
+// or
+console.log('true || true',true || true)
+console.log('true || false',true || false)
+console.log('false || true',false || true)
+console.log('false || false',false || false)
