56fa56bbc6e5dd59be39d9523a134da523defd12
Mrunal-09 <gosavimrunal36@gmail.com>
2021-10-24 11:41:17 +0530

    for loop

--- app.js
+++ app.js
@@ -1,23 +1,12 @@
-//DOM is accessing html by js
-var heading=document.getElementById('heading')
 
-heading.innerHTML="hello"
-var para=document.getElementsByClassName('para')
-var tags=document.getElementsByTagName('a')
-//it wil return all anchor in html
-var query=document.querySelectorAll('.para')
-var body=document.querySelector('body')
-
-console.log(heading)
-console.log(para.length)
-// length is how many classes of same name
-console.log(para[0])
-// [0] is return first class
-console.log(para[1])
-console.log(tags)
-console.log(query)
-heading.style="color:red;font-size:2rem"
-console.log(heading.classList)
-heading.classList.add("six")
-heading.classList.remove('one')
-body.classList.add("dark")
+var list=document.querySelector('ul')
+var arr=['four ','three','two']
+arr.push("go there")
+for(var i=0;i<arr.length;i++){
+  
+  var element=document.createElement('li')
+  var textNode=document.createTextNode(arr[i])
+  element.appendChild(textNode)
+  list.appendChild(element)
+  
+}
