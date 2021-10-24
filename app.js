01bb43bb700106d4a1a3eeca4f5d2d4e4d1c7160
Mrunal-09 <gosavimrunal36@gmail.com>
2021-10-24 23:02:14 +0530

    functions

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
-
-var list=document.querySelector('ul')
-var arr=['four ','three','two']
-arr.push("go there")
-for(var i=0;i<arr.length;i++){
-  
-  var element=document.createElement('li')
-  var textNode=document.createTextNode(arr[i])
-  element.appendChild(textNode)
-  list.appendChild(element)
-  
+//function
+function greet(operation,a,b){
+  console.log(operation,a+b)
 }
+greet('addition',1,2)
+
+function hi(Name='John',age=19){
+  console.log("Hey my name is "+ Name +" and my age is "+ age +".")
+}
+hi(Name='Mrunal',age=20)
+//nan=not a number
+var add=function(a=1,b=2){
+  //function expression
+  var sum=a+b;
+  console.log(sum)
+}
+add()
+//diff between funtion expression and declaration is that func called before or after ..decl prints before also
+//expression>declaration
+
+var add=(a=1,b=2)=>{
+  //function expression
+  var sum=a+b;
+  console.log(sum)
+}
+add()
+//arrow function
