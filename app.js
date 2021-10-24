b30121c73a1323710544be45668fef9aca439a2a
Mrunal-09 <gosavimrunal36@gmail.com>
2021-10-24 10:51:00 +0530

    Dom methods

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ heading.innerHTML="hello"
 var para=document.getElementsByClassName('para')
 var tags=document.getElementsByTagName('a')
 //it wil return all anchor in html
-var query=document.querySelector('')
+var query=document.querySelectorAll('.para')
+var body=document.querySelector('body')
 
 console.log(heading)
 console.log(para.length)
@@ -14,3 +15,9 @@ console.log(para[0])
 // [0] is return first class
 console.log(para[1])
 console.log(tags)
+console.log(query)
+heading.style="color:red;font-size:2rem"
+console.log(heading.classList)
+heading.classList.add("six")
+heading.classList.remove('one')
+body.classList.add("dark")
