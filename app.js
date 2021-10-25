d675ee408de8be1790cd08979d56c0809690f0fe
Mrunal-09 <gosavimrunal36@gmail.com>
2021-10-25 12:41:43 +0530

    dark mode

--- app.js
+++ app.js
@@ -1,21 +1,8 @@
 var button=document.querySelector('button')
-var input=document.querySelector('input')
-var list=document.querySelector('ul')
+var body=document.querySelector('body')
 
-//const callbackfunc = (event) =>{
-  //console.log(event.target)
-  //console.log(input.value) 
-  //adds input on click in console
-//}
-
-//IMP TO DO LIST
 const callbackfunc=(event) =>{
-  const inputvalue=input.value
-  const element=document.createElement('li')
-  const textnode=document.createTextNode(inputvalue)
-  element.appendChild(textnode)
-  list.appendChild(element)
-  console.log(input.value)
+  body.classList.add('dark')
 }
 
 button.addEventListener('click',callbackfunc)
