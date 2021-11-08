8f2e5bd64dd2086cf3cf7ec7beae3ce00b799976
Mrunal-09 <gosavimrunal36@gmail.com>
2021-11-08 20:48:34 +0530

    splice method

--- app.js
+++ app.js
@@ -1,22 +1,5 @@
 //array methods
 
 var arr=[1,2,3,4]
-arr.push(5)
-arr.shift()
-arr.unshift(6)
-//shift removes first element
-//unshift adds to first position
-
+arr.splice(1,2)
 console.log(arr)
-console.log(arr.slice(2,4))
-var arr1=[]
-for (var i=0;i<arr.length;i++ ){
-  if(arr[i]%2==0)
-  arr1.push(arr[i])
-}
-console.log(arr1)
-var arr1=arr.filter((element,index)=>{
-  //console.log(element,index)
-  if(element>5) return true
-})
-console.log(arr1)
