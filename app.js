6f035a6cf677c22aed0e4eb96a8356920b23a6d0
Mrunal-09 <gosavimrunal36@gmail.com>
2021-11-08 20:38:30 +0530

    array methods

--- app.js
+++ app.js
@@ -1,64 +1,22 @@
-const a=document.querySelectorAll('input')[0]
-const b=document.querySelectorAll('input')[1]
+//array methods
 
-const add=document.querySelector('#add')
-const subtract=document.querySelector('#subtract')
-const multiply=document.querySelector('#multiply')
-const divide=document.querySelector('#divide')
+var arr=[1,2,3,4]
+arr.push(5)
+arr.shift()
+arr.unshift(6)
+//shift removes first element
+//unshift adds to first position
 
-const resultBox=document.querySelector('.result')
-
-
- /*const sum= () => {
-  const result=parseInt(a.value) + parseInt(b.value)
-  resultBox.innerHTML = result
+console.log(arr)
+console.log(arr.slice(2,4))
+var arr1=[]
+for (var i=0;i<arr.length;i++ ){
+  if(arr[i]%2==0)
+  arr1.push(arr[i])
 }
-const minus= () => {
-  const result=parseInt(a.value) - parseInt(b.value)
-  resultBox.innerHTML = result
-}*/
-
-const calculate =(event,operation)=>{
-  /*console.log(event.target)
-  if (operation=='add'){
-    console.log('adding')
-  }else if (operation =='subtract'){
-    console.log('subtract')
-  }*/
-  switch(operation){
-    case "add":
-      resultBox.innerHTML=parseInt(a.value) + parseInt(b.value)
-      break
-      case "subtract":
-      resultBox.innerHTML=parseInt(a.value) - parseInt(b.value)
-      break
-      case "multiply":
-      resultBox.innerHTML=parseInt(a.value) * parseInt(b.value)
-      break
-      case "divide":
-      resultBox.innerHTML=parseInt(a.value) / parseInt(b.value)
-      break
-      default:
-        resultBox.innerHTML="Not a valid operation"
-  }
-}
-//calculate is a dummy function so that event listener uses it indirectly
-
-add.addEventListener('click',(event) =>{
-  calculate(event,'add')
+console.log(arr1)
+var arr1=arr.filter((element,index)=>{
+  //console.log(element,index)
+  if(element>5) return true
 })
-subtract.addEventListener('click',(event) =>{
-  calculate(event,'subtract')
-})
-multiply.addEventListener('click',(event) =>{
-  calculate(event,'multiply')
-})
-divide.addEventListener('click',(event) =>{
-  calculate(event,'divide')
-})
-
-
-
-
-
-
+console.log(arr1)
