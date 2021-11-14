38fc2ae94b3315c43f7742a7f3ff580724b211e0
Mrunal-09 <gosavimrunal36@gmail.com>
2021-11-14 13:42:28 +0530

    promises

--- app.js
+++ app.js
@@ -1,54 +1,24 @@
-var a=22
-let b=24
-console.log(a)
-console.log(b)
-
-if (true){
-  let Name="Ferrari"
-  console.log(Name)
-}
-//console.log(Name)
-//let variable has scope only where its declared
-//var vriable has global scope
-
-for(var i=0;i<10;i++){
-  console.log(i)
-}
-console.log(i)
-if (true){
-  let Name="Ferrari"
-  if(true){
-    console.log(Name)
+//object methods
+let car={ //car is a object
+  name:"Alto",
+  print:()=>{
+    console.log(car.name)
   }
 }
-const print =()=>{
-  var x="xyz"
+console.log(Object.keys(car))
+console.log(Object.values(car))
+console.log(Object)
+console.log(car.hasOwnProperty('name'))
 
-  if(true){
-    console.log(x)
-}
-return x
-}
-//console.log(x)
-print()
-//global scope not valid for functions
-console.log(this)
-//gives alert to window(present parent object)
-console.log(this.alert("Hi"))
+//promises
+// promise is a object..in js everyting is a object
 
-let car={
- name:"Alto",
- manufacturer:"Benz",
- //print : function(a){ //this. car. both works
-  print: (a) =>{ //this. doesnt works
-   //console.log(a)
-   console.log(car.name+" was manufactured by "+ car.manufacturer+".")
-   //console.log(this.manufacturer)..same as car.
- }
-}
-console.log(car)
-console.log(car.name)
-console.log(typeof car)//object
+//settimeout
+setTimeout(()=>{
+    console.log("kiran")
+},5000)
+//1 sec=1000
+console.log("hi")
 
-car.print()
-
+var data=fetch("https://jsonplaceholder.typicode.com")
+console.log(data)
