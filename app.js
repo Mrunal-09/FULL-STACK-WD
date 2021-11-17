7f19b590f6b74bbe2a3831db1f12e73d27e97726
Mrunal-09 <gosavimrunal36@gmail.com>
2021-11-17 14:32:30 +0530

    fetch and display api data

--- app.js
+++ app.js
@@ -1,22 +1,34 @@
-//promises
-//it does not need a callback
-//created using constructor
 /*let value=new Promise((resolve,reject)=>(
-   setTimeout(()=>{
-     resolve("Hello")
+  setTimeout(()=>{
+    resolve("Mrunal Gosavi")
+  },4000)
+)).then(value=>{console.log(value)})
 
-   },3000)
-))
-console.log(value)
-setTimeout(()=>{
-  console.log(value)
-},4000)*/
+//.then gives data once promise fullfilled,only promise does not gives data*/
 
-/*let response=fetch("https://jsonplaceholder.typicode.com/users").then(data=>{
-console.log(data) //gives response no data
-let parsedata=data.json()
-console.log(parsedata)//returns promise with data
-return parsedata
-}).then(data=>{console.log(data)})//gives data*/
+const table=document.querySelector('table')
+console.log(table)
 
-let response=fetch("https://jsonplaceholder.typicode.com/users").then(data=> data.json()).then(data=>{console.log(data)})
+fetch("https://jsonplaceholder.typicode.com/users")
+.then(value=>value.json())
+.then(data=>{
+  console.log(data)
+//gives whole data
+data.forEach(user => {
+  //console.log(user)
+  let newRow=document.createElement('tr')
+  /*let dataID=document.createElement('td')
+  let dataIDtextnode=document.createTextNode(user.id)//to display id ...similar will be done for name,gmail,username but not feasible
+  dataID.appendChild(dataIDtextnode)
+  newRow.appendChild(dataID)*/
+  //so backtick is used``
+  let element= `  
+  <td>${user.id}</td>
+  <td>${user.name}</td>
+  <td>${user.email}</td>
+  <td>${user.username}</td>`
+  //appendchild method only accepts node,node is object,newRow is string so cannot be appended,ie y innerhtml is used
+  newRow.innerHTML=element
+  table.appendChild(newRow)
+})
+})
