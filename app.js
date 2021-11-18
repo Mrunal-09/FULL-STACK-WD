839a458b0f1368355ad26a899ed77b7920c77d51
Mrunal-09 <gosavimrunal36@gmail.com>
2021-11-18 07:38:56 +0530

    async,await

--- app.js
+++ app.js
@@ -1,45 +1,18 @@
-/*let value=new Promise((resolve,reject)=>(
-  setTimeout(()=>{
-    resolve("Mrunal Gosavi")
-  },4000)
-)).then(value=>{console.log(value)})
-
-//.then gives data once promise fullfilled,only promise does not gives data*/
-
-const table=document.querySelector('table')
-console.log(table)
-
-const createRow=(user)=>{
-  let newRow=document.createElement('tr')
-  let element= `  
-  <td>${user.id}</td>
-  <td>${user.name}</td>
-  <td>${user.email}</td>
-  <td>${user.username}</td>`
-  newRow.innerHTML=element
-  return newRow
-}
-
-fetch("https://jsonplaceholder.typicode.com/users")
+/*fetch("https://jsonplaceholder.typicode.com/users")
 .then(value=>value.json())
 .then(data=>{
   console.log(data)
 //gives whole data
 data.forEach(user => {
-  //console.log(user)
-  //let newRow=document.createElement('tr')
-  /*let dataID=document.createElement('td')
-  let dataIDtextnode=document.createTextNode(user.id)//to display id ...similar will be done for name,gmail,username but not feasible
-  dataID.appendChild(dataIDtextnode)
-  newRow.appendChild(dataID)*/
-  //so backtick is used``
-  /*let element= `  
-  <td>${user.id}</td>
-  <td>${user.name}</td>
-  <td>${user.email}</td>
-  <td>${user.username}</td>`*/
-  //appendchild method only accepts node,node is object,newRow is string so cannot be appended,ie y innerhtml is used
-  //newRow.innerHTML=element
-  table.appendChild(createRow(user))
+  console.log(user)
+  
 })
-})
+})*/
+
+const fetchdata=async()=>{
+  const response=await fetch("https://jsonplaceholder.typicode.com/users")
+  console.log(response)
+  const data=await response.json
+  console.log(data)
+}
+fetchdata()
