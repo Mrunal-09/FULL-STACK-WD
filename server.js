342f2f2d61a34bcab1327973b431ae7ec81be6d2
Mrunal-09 <gosavimrunal36@gmail.com>
2021-12-28 13:00:03 +0530

    routes

--- server.js
+++ server.js
@@ -1,23 +1,20 @@
-//creating server
+//Routes
 const http=require("http")
 const fs =require("fs")
 const path =require('path')
-//console.log(http)
+
 const server=http.createServer((request,response)=>{
-  //console.log(request.method)
-  //const path=request.url
-  //response.write("hi")
-  //response.write(`You made a request to ${path}`)//gives path
-  //response.write(`<h1> I'm a heading </h1>`)//sending html to server
-  const markup=fs.readFileSync(path.resolve('./index.html'))
-  response.write(markup)
-  response.end()
-})//it creates server
-//request.url
-//request.headers
-//request.methods
+  const {url}=request
+  if (url=='/login'){
+    response.write(`<h1>Login</h1>`)
+    response.end()
+  }
+  if (url=='/signup'){
+    response.write(`<h1>Sign Up</h1>`)
+    response.end()
+  }
+})//if you add /login to localhost:3000 response will be login 
 
 server.listen(3000,()=>{
   console.log(`Server Listening at PORT: $(3000)`)
-})//to start server
-//http is built in nodejs module,
+})
